0a01af77e1705d367f67d9e1cd309a4072c678f4
AlekseyBelyaev2606 <partchoice44@gmail.com>
2023-01-31 23:02:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите  число");
+int N = int.Parse(Console.ReadLine());
+string Value = Convert.ToString(N);
+if (Value.Length != 3){
+    Console.WriteLine("Вы ввели не правильное число");
+}
+else{
+    Console.WriteLine(Value[1]);
+}
+int n1 = N/10;
+int n2 = n1 % 10;
+
+Console.WriteLine(n2);
+
+
